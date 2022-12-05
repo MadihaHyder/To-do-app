023ed4b670677cd71b83a7d7e0d099c73be0c6ac
Madiha Hyder <116061555+MadihaHyder@users.noreply.github.com>
2022-12-05 15:55:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,14 +25,14 @@ function vals(e)
 
 		task_content_el.appendChild(task_input_el);
 
-		const task_actions_el = document.createElement('div');
+		let task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
 		
-		const task_edit_el = document.createElement('button');
+		let task_edit_el = document.createElement('button');
 		task_edit_el.classList.add('edit');
 		task_edit_el.innerText = 'Edit';
 
-		const task_delete_el = document.createElement('button');
+		let task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
 
